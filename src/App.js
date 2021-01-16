b73f19713de0294529455133798621e16b3b16b8
danilka7178 <danya.sidorow@yandex.ru>
2021-01-16 07:22:17 +0300

    fix count, need vision

--- src/App.js
+++ src/App.js
@@ -6,17 +6,14 @@ function App() {
   const [data, setData] = React.useState("");
   const [count, setCount] = React.useState(1);
 
+
   React.useEffect(() => {
     axios.get(`https://5c3755177820ff0014d92711.mockapi.io/users?page=${count}&limit=10`)
       .then(({ data }) => {
         setData(data);
       });
-  }, []);
+  }, [count]);
 
-  function handeClick() {
-    let mutableState = Object.assign({}, count);
-    setCount(mutableState);
-  };
 
   return (
     <div className="app">
@@ -24,7 +21,7 @@ function App() {
       <ul className="users">
         {data ? data.map((arg) => (<Card key={`${arg.id}_${arg.name.toString()}`} name={arg.name} email={arg.email} />)) : "Загрузка"}
       </ul>
-      <button onClick={e => handeClick(e)}>Next 10 users</button>
+      <button onClick={() => setCount(count + 1)}>Next 10 users</button>
     </div >
   );
 }
